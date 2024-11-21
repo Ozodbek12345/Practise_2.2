3207dafd50ff5eb04ab78afb51ed55459d8cf82d
notaprogramer01 <mr.creeper2003@bk.ru>
2024-11-21 11:58:22 +0300

    Коммит после merge

--- Practise_2.2.py
+++ Practise_2.2.py
@@ -6,12 +6,13 @@ def speed():
     v = s/t
     print("Скорость равна {0} км/ч".format(round(v, 2)))
 
-
 def frequency():
     print("Введите период колебаний в секундах")
     T = float(input())
     v = 1 / T
     print("Частота колебаний равна {0} Гц".format(round(v, 2)))
+
+
 print("Введите число для выбора задачи")
 print("Задача 1: Определение скорости")
 print("Задача 2: Определение массы")
@@ -23,9 +24,15 @@ print("Задача 7: Определение давления")
 print("Задача 8: Определение теплоты")
 print("Задача 9: Определение частоты")
 print("Задача 10: Определение объема")
-number = int(input())
-if number == 1:
-    speed()
 
-if number == 9:
-    frequency()
+# functions = [speed, func2, func3, func4, func5, func6, func7, func8, func9, frequency]
+functions = [speed, ]
+
+number = int(input())
+functions[number - 1]()
+
+# if number == 1:
+#     speed()
+#
+# if number == 9:
+#     frequency()
